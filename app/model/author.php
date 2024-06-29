dcadc6530c42a016879b4a5b9adf9447647e6e96
SamhainV <antoniomartinezramirez@gmail.com>
2024-06-30 01:48:41 +0200

    1

--- app/model/author.php
+++ app/model/author.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once(BASE_DIR . 'dao/implementations/authorDAO.php');
+//require_once(BASE_DIR . 'dao/implementations/authorDAO.php');
 
 class Author
 {
@@ -10,7 +10,7 @@ class Author
     public $Original_members; // Número de miembros originales de la banda
     public $Breakup_date; // Año de separación de la banda, si aplica
     public $db;
-    public $authorDAO;
+    //public $authorDAO;
 
     // Constructor
     public function __construct(
@@ -19,38 +19,72 @@ class Author
         $Author_Name = null,
         $Debut_album_release = null,
         $Original_members = null,
-        $Breakup_date = null,        
+        $Breakup_date = null,
     ) {
         $this->db = $db;
         $this->Id = $Id;
         $this->Author_Name = $Author_Name;
         $this->Debut_album_release = $Debut_album_release;
         $this->Original_members = $Original_members;
-        $this->Breakup_date = $Breakup_date;        
-        $this->authorDAO = new AuthorDAO($db);
+        $this->Breakup_date = $Breakup_date;
+        //$this->authorDAO = new AuthorDAO($db);
     }
 
     // Añade un autor
     public function addAuthor(Author $author)
     {
-        return $this->authorDAO->insert($author);
+        $sql = "INSERT INTO AUTHORS_TBL (Author_Name, Debut_album_release, Original_members, Breakup_date) VALUES (?, ?, ?, ?)";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute([$author->Author_Name, $author->Debut_album_release, $author->Original_members, $author->Breakup_date]);
+        $author->Id = $this->db->lastInsertId();
+        return $author->Id; // Devuelve el ID del autor insertado
     }
 
     // Obtiene todos los autores
-    public function getAllAuthors() 
+    public function getAllAuthors()
     {
-        return $this->authorDAO->findAll();
+        $sql = "SELECT * FROM AUTHORS_TBL";
+        $stmt = $this->db->query($sql);
+        return $stmt->fetchAll(PDO::FETCH_CLASS, 'Author'); // Devuelve un array de objetos Author
+//        return $this->authorDAO->findAll();
     }
- 
+
     // Obtiene un autor por ID
     public function getAuthorById($id)
     {
-        return $this->authorDAO->findById($id);
+        $sql = "SELECT * FROM AUTHORS_TBL WHERE Id = ?";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute([$id]);
+        $stmt->setFetchMode(PDO::FETCH_CLASS, 'Author');
+        return $stmt->fetch(); // Devuelve un objeto Author
+        //return $this->authorDAO->findById($id);
     }
 
     // actualiza un autor
     public function updateAuthor(Author $author)
     {
-        return $this->authorDAO->update($author);
+        if (empty($author->Id)) {
+            throw new InvalidArgumentException("El ID del autor es necesario para actualizar.");
+        }
+
+        $sql = "UPDATE AUTHORS_TBL SET 
+                Author_Name = ?, 
+                Debut_album_release = ?, 
+                Original_members = ?, 
+                Breakup_date = ?
+                WHERE Id = ?";
+
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute([
+            $author->Author_Name,
+            $author->Debut_album_release,
+            $author->Original_members,
+            $author->Breakup_date,
+            $author->Id
+        ]);
+
+        // Devuelve el número de filas afectadas
+        return $stmt->rowCount();
+        //return $this->authorDAO->update($author);
     }
 }
